43346cbab19fdb825320db9a05d5a8b572dd9fc8
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 03:29:54 -0500

    remove force from example

--- examples/ask.js
+++ examples/ask.js
@@ -4,7 +4,7 @@ var Questions = require('..');
 var questions = new Questions();
 
 questions
-  .set('name', 'What is the project name?', {force: true})
+  .set('name', 'What is the project name?')
   .set('desc', 'What is the project description?', {force: true});
 
 questions
