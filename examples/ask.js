567660939e30764f83835ef748723212d9589620
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-07 01:54:46 -0500

    update example

--- examples/ask.js
+++ examples/ask.js
@@ -3,9 +3,14 @@
 var Questions = require('..');
 var questions = new Questions();
 
+questions.option('init', 'name');
 questions
-  .set('name', 'What is the project name?')
-  .set('desc', 'What is the project description?', {force: true});
+  .set('author.name', 'Author name?')
+  .set('author.username', 'Author username?')
+  .set('author.url', 'Author url?')
+
+  .set('project.name', 'What is the project name?')
+  .set('project.desc', 'What is the project description?', {force: true});
 
 questions
   .ask(function(err, answer) {
